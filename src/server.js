805e86b9a503faeb20937f01d10a080a8a8c5b60
Riccardo Balbo <riccardo0blb@gmail.com>
2024-05-14 17:00:05 +0200

    Add ssl

--- src/server.js
+++ src/server.js
@@ -1,32 +1,46 @@
 import Express from 'express';
+import https from 'https';
+import fs from 'fs';
+
+const SERVER_CRT = process.env.SERVER_CRT;
+const SERVER_KEY = process.env.SERVER_KEY;
 
 const app = Express();
 app.use(Express.json());
 
-let EVENTS=[];
+let EVENTS = [];
 
-app.post("/post", (req, res) => {   
+app.post("/post", (req, res) => {
     const event = req.body;
-    const secret=req.query.secret;
+    const secret = req.query.secret;
     EVENTS.push({
         event: event,
         timestamp: new Date(),
         secret: secret
     });
-    res.json({status: "ok"});
+    res.json({ status: "ok" });
 });
 
 app.get("/get", (req, res) => {
-    const secret=req.query.secret;
-    EVENTS=EVENTS.filter((event) => {
-        return new Date() - event.timestamp < 1000*60*60;
+    const secret = req.query.secret;
+    EVENTS = EVENTS.filter((event) => {
+        return new Date() - event.timestamp < 1000 * 60 * 60;
     });
     res.json(EVENTS.filter((event) => {
         return event.secret === secret;
     }).map((event) => event.event));
 });
 
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+if (SERVER_CRT && SERVER_KEY) {
+    const options = {
+        key: fs.readFileSync(SERVER_KEY),
+        cert: fs.readFileSync(SERVER_CRT)
+    };
+    https.createServer(options, app).listen(3000, () => {
+        console.log("HTTPS Server is running on port 3000");
+    });
+} else {
+    app.listen(3000, () => {
+        console.log("HTTP Server is running on port 3000");
+    });
+}
